df3effeea4167720949a1c47e9b7432dcff050ed
Felipe Marques Batista <felipefmb@gmail.com>
2024-03-23 17:29:54 -0300

    feat: criado projeto endpoint

---
+++ endpoint/endpoint/tests/test_app.py
@@ -0,0 +1,7 @@
+from chalice.test import Client
+from app import app
+
+def test_index():
+    with Client(app) as client:
+        response = client.http.get('/id/12345')
+        assert response.json_body == True
